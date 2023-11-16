c0d72382dce02a41a8c064d14f475224bceae6e8
aqew78 <ggfdagwshghxfg@gmail.com>
2023-11-16 23:36:22 +0800

    Initial commit

---
+++ src/test/java/com/example/gitactionDependencyCheck/GitactionDependencyCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactionDependencyCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitactionDependencyCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
